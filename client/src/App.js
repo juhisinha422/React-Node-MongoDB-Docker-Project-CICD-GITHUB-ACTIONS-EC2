4a0c235fc6ca51040f6d81efc5a623261227ca11
Ubuntu <ubuntu@ip-172-31-28-38.ec2.internal>
2026-01-10 07:25:59 +0000

    Update frontend UI and content

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
+import "./App.css";
+
 function App() {
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>React Node MongoDB CI/CD 🚀</h1>
+    <div className="app">
+      <h1>🚀 MERN CI/CD Deployment</h1>
+      <p>
+        React + Node.js + MongoDB deployed on AWS EC2
+      </p>
+
+      <div className="info">
+        <h3>Tech Stack</h3>
+        <ul>
+          <li>React (Frontend)</li>
+          <li>Node.js & Express (Backend)</li>
+          <li>MongoDB Atlas (Database)</li>
+          <li>Docker & Docker Compose</li>
+          <li>GitHub Actions (CI/CD)</li>
+          <li>AWS EC2 (Deployment)</li>
+        </ul>
+      </div>
+
+      <footer>
+        <p>Hello Everyone!!!
+	  Built by Juhi Sinha 💻
+	  How are you?</p>
+      </footer>
     </div>
   );
 }
+
 export default App;
 
